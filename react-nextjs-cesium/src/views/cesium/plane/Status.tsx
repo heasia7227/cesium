3d2a46f01c14d0ea2a1d5f03a3b74bb89a6cc09e
Arthur <108118358+heasia7227@users.noreply.github.com>
2025-04-03 09:03:38 +0800

    模拟飞机故障

--- react-nextjs-cesium/src/views/cesium/plane/Status.tsx
+++ react-nextjs-cesium/src/views/cesium/plane/Status.tsx
@@ -1,18 +1,28 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import _ from "lodash";
 
 interface IProps {
     show: boolean;
 }
 
 const PlaneStatus = ({ show }: IProps) => {
+    const [scene3D, setScene3D] = useState<THREE.Scene>();
+    const [errorMessage, setErrorMessage] = useState<string>("");
+
     useEffect(() => {
         displayPlaneStatus();
     }, []);
+
+    useEffect(() => {
+        if (show && scene3D) {
+            testException();
+        }
+    }, [scene3D, show]);
 
     const displayPlaneStatus = () => {
         let mixer: THREE.AnimationMixer; // 动画混合器
@@ -24,6 +34,7 @@ const PlaneStatus = ({ show }: IProps) => {
 
         // 3D场景
         const scene = new THREE.Scene();
+        setScene3D(scene);
 
         // 环境光
         const ambient = new THREE.AmbientLight(0xffffff, 2);
@@ -34,6 +45,16 @@ const PlaneStatus = ({ show }: IProps) => {
         loader.load("/gltf/Cesium_Air.glb", function (gltf) {
             const model = gltf.scene;
             scene.add(model);
+
+            model.traverse((item: any) => {
+                if (item.isMesh) {
+                    item.material = new THREE.MeshLambertMaterial({
+                        color: 0x00ff00,
+                        transparent: true,
+                        opacity: 0.5,
+                    });
+                }
+            });
 
             // 获取动画数据
             const animations = gltf.animations;
@@ -82,6 +103,20 @@ const PlaneStatus = ({ show }: IProps) => {
         }
     };
 
+    const testException = () => {
+        _.delay(() => {
+            const planePart = scene3D?.getObjectByName("Prop__2_");
+            planePart?.traverse((item: any) => {
+                item.material = new THREE.MeshLambertMaterial({
+                    color: 0xff0000,
+                    transparent: true,
+                    opacity: 0.5,
+                });
+            });
+            setErrorMessage("左侧发动机故障！");
+        }, 5000);
+    };
+
     return (
         <>
             <div
@@ -89,6 +124,14 @@ const PlaneStatus = ({ show }: IProps) => {
                 className="absolute w-[300px] h-[300px] top-[60px] right-1 z-[1000]"
                 style={{ display: show ? "" : "none" }}
             ></div>
+            {errorMessage && (
+                <span
+                    className="absolute top-[60px] right-1 z-[1001] text-red-500"
+                    style={{ display: show ? "" : "none" }}
+                >
+                    {errorMessage}
+                </span>
+            )}
         </>
     );
 };
